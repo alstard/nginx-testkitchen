fcb31536029c72246880b8b712490a2cee214e21
alstard <alastair.dickson@gmail.com>
2018-01-16 12:54:13 +0000

    Linter fixes

--- spec/ntp_spec.rb
+++ spec/ntp_spec.rb
@@ -1,19 +1,19 @@
 require_relative 'spec_helper'
 
 describe 'ntp site.yml' do
-    describe package('ntp') do
-      it { should be_installed }
-    end
-  
-    describe file('/etc/ntp.conf') do
-      its(:content) { should match(/^server [0-9].uk.pool.ntp.org iburst$/) }
-    end
-  
-    describe service('ntpd') do
-      it { should be_enabled }
-    end
-  
-    describe service('ntpd') do
-      it { should be_running }
-    end
+  describe package('ntp') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/ntp.conf') do
+    its(:content) { should match(/^server [0-9].uk.pool.ntp.org iburst$/) }
+  end
+
+  describe service('ntpd') do
+    it { should be_enabled }
+  end
+
+  describe service('ntpd') do
+    it { should be_running }
+  end
 end
